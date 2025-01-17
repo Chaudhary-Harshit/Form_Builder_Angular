069549f924256ad20adb2707081003e32f7b88ce
Harshit Chaudhary <harshitchaudhary@Harshits-MacBook-Air.local>
2025-01-17 11:48:58 +0530

    added ngOnInit

--- src/app/markup-viewer/markup-viewer.component.ts
+++ src/app/markup-viewer/markup-viewer.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';  
+
 
 @Component({
   selector: 'app-markup-viewer',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './markup-viewer.component.html',
-  styleUrls: ['./markup-viewer.component.css'],
+  styleUrls: ['./markup-viewer.component.css']
 })
-export class MarkupViewerComponent {
-  htmlMarkup: string = '';
+export class MarkupViewerComponent implements OnInit {
+  htmlMarkup: string | undefined;
 
-  constructor(private router: Router) {
-    const navigation = this.router.getCurrentNavigation();
-    this.htmlMarkup = navigation?.extras.state?.['html'] || 'No HTML generated!';
+  constructor() { }
+
+  ngOnInit(): void {
+    // Retrieve the HTML markup from the navigation state
+    const rawMarkup = history.state.html;
+
+    // Set the raw HTML markup to be displayed as text
+    this.htmlMarkup = rawMarkup;
+    console.log('Received HTML Markup:', this.htmlMarkup);
   }
 }
